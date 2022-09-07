ca8f732955d00139ad36bb7576452a0898e8018f
Abhay Kashyap <71137700+abhay2008@users.noreply.github.com>
2022-09-07 12:15:00 +0530

    Add files via upload

---
+++ js/Car.js
@@ -0,0 +1,14 @@
+AFRAME.registerComponent("car-model",{
+    schema:{
+        modelRef:{type:"string", default:"../assets/car/car.glb"}
+    },
+
+    init: function(){
+        this.el.setAttribute("gltf-model", this.data.modelRef);
+        const position = {x:0, y:40, z:30};
+        const rotation = {x:0, y:-100, z:0};
+        this.el.setAttribute("position", position);
+        this.el.setAttribute("rotation", rotation);
+    }
+
+});
